f89bf59dbfb1758950dfa99e83addb3cf055d9e3
Alexandru Benzari <benzari.alex@gmail.com>
2015-12-25 13:46:46 +0200

    - add travis build status to readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # XabenMediaBundle
 Bundle for managing images
+
+[![Build Status](https://secure.travis-ci.org/xaben/XabenMediaBundle.svg)](http://travis-ci.org/xaben/XabenMediaBundle)
--- Tests/autoload.php
+++ Tests/autoload.php
@@ -1,6 +1,5 @@
 <?php
 
-//use Doctrine\Common\Annotations\AnnotationRegistry;
 use Composer\Autoload\ClassLoader;
 
 error_reporting(error_reporting() & ~E_USER_DEPRECATED);
@@ -9,7 +8,5 @@ error_reporting(error_reporting() & ~E_USER_DEPRECATED);
  * @var ClassLoader $loader
  */
 $loader = require __DIR__.'/../vendor/autoload.php';
-
-//AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
 return $loader;
