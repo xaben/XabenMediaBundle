9e0e8167acba65a10709e4a2fa2d7813d3b705d9
Alexandru Benzari <benzari.alex@gmail.com>
2015-12-25 12:30:46 +0200

    - add more test assertions

--- Tests/Locator/DefaultMediaLocatorTest.php
+++ Tests/Locator/DefaultMediaLocatorTest.php
@@ -38,6 +38,34 @@ class DefaultMediaLocatorTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($referencePath, $locator->getReferencePath($media));
     }
 
+    /**
+     * @dataProvider getFixtures
+     * @param $id
+     * @param $context
+     * @param $format
+     * @param $reference
+     * @param $referencePath
+     * @param $thumbnailPath
+     */
+    public function testReturnsCorrectThumbnailPath($id, $context, $format, $reference, $referencePath, $thumbnailPath)
+    {
+        $media = $this->getMockBuilder('Xaben\MediaBundle\Model\Media')
+            ->setMethods(array('getId', 'getReference', 'getContext'))
+            ->getMock();
+        $media
+            ->expects($this->any())
+            ->method('getId')
+            ->will($this->returnValue($id));
+        $media->expects($this->once())
+            ->method('getContext')
+            ->will($this->returnValue($context));
+
+        $locator = new DefaultMediaLocator();
+
+        $this->assertEquals($thumbnailPath, $locator->getThumbnailPath($media, $format));
+    }
+
+
     public static function getFixtures()
     {
         return array(
@@ -46,28 +74,32 @@ class DefaultMediaLocatorTest extends \PHPUnit_Framework_TestCase
                 'context' => 'mycontext',
                 'format' => 'test',
                 'reference' => 'testReference',
-                'referencePath' => 'uploads/mycontext/reference/0001/01/1_testReference'
+                'referencePath' => 'uploads/mycontext/reference/0001/01/1_testReference',
+                'thumbnailPath' => 'uploads/mycontext/thumbs/test/0001/01/thumb_1.png',
             ),
             array(
                 'id' => 1999,
                 'context' => 'context',
                 'format' => 'test',
                 'reference' => 'testReference',
-                'referencePath' => 'uploads/context/reference/0001/02/1999_testReference'
+                'referencePath' => 'uploads/context/reference/0001/02/1999_testReference',
+                'thumbnailPath' => 'uploads/context/thumbs/test/0001/02/thumb_1999.png',
             ),
             array(
                 'id' => 10999,
                 'context' => 'context',
                 'format' => 'test',
                 'reference' => 'testReference',
-                'referencePath' => 'uploads/context/reference/0001/11/10999_testReference'
+                'referencePath' => 'uploads/context/reference/0001/11/10999_testReference',
+                'thumbnailPath' => 'uploads/context/thumbs/test/0001/11/thumb_10999.png',
             ),
             array(
                 'id' => 100999,
                 'context' => 'context',
                 'format' => 'test',
                 'reference' => 'testReference',
-                'referencePath' => 'uploads/context/reference/0002/01/100999_testReference'
+                'referencePath' => 'uploads/context/reference/0002/01/100999_testReference',
+                'thumbnailPath' => 'uploads/context/thumbs/test/0002/01/thumb_100999.png',
             )
         );
     }
